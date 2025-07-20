adfb17733a87fe27e39be482acc31566102f0efa
Emre Yapici <emre4821@outlook.com>
2025-07-20 20:02:36 +0300

    Create CorsConfig.java

---
+++ backend/bookstorium/src/main/java/com/books/bookstorium/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.books.bookstorium.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Tüm endpointler için geçerli
+                        .allowedOrigins("https://bookstorium.vercel.app") // Frontend adresin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
